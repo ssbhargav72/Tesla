bb76471aeb981282784086e189214d9eb36c88e2
ssbhargav72 <139240994+ssbhargav72@users.noreply.github.com>
2023-08-30 22:46:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,44 +19,44 @@ app.use('/static',express.static('static'))
 app.get('/', (req, res)=>{
     //const params = { }
     // res.status(200).sendFile(path.join("home.html");
-    res.sendFile(path.join(__dirname ,'../ev','/views','model.html'));
+    res.sendFile(path.join(__dirname ,'../ev','views','model.html'));
 })
 
 app.get('/vehicles', (req, res)=>{ 
     // const params = { }
     // res.status(200).end(aboutus);
-    res.sendFile(path.join(__dirname ,'../ev','/views','vehicles.html'));
+    res.sendFile(path.join(__dirname ,'../ev','views','vehicles.html'));
 })
 
 app.get('/energy', (req, res)=>{ 
     // const params = { }
     // res.status(200).end(aboutus);
-    res.sendFile(path.join(__dirname ,'../ev','/views','energy.html'));
+    res.sendFile(path.join(__dirname ,'../ev','views','energy.html'));
 })
 app.get('/charging', (req, res)=>{ 
     // const params = { }
     // res.status(200).end(aboutus);
-    res.sendFile(path.join(__dirname ,'../ev','/views','charging.html'));
+    res.sendFile(path.join(__dirname ,'../ev','views','charging.html'));
 })
 app.get('/order-mod3', (req, res)=>{ 
     // const params = { }
     // res.status(200).end(aboutus);
-    res.sendFile(path.join(__dirname ,'../ev','/views','order-mod3.html'));
+    res.sendFile(path.join(__dirname ,'../ev','views','order-mod3.html'));
 })
 app.get('/order-mods', (req, res)=>{ 
     // const params = { }
     // res.status(200).end(aboutus);
-    res.sendFile(path.join(__dirname ,'../ev','/views','order-mods.html'));
+    res.sendFile(path.join(__dirname ,'../ev','views','order-mods.html'));
 })
 app.get('/order-modx', (req, res)=>{ 
     // const params = { }
     // res.status(200).end(aboutus);
-    res.sendFile(path.join(__dirname ,'../ev','/views','order-modx.html'));
+    res.sendFile(path.join(__dirname ,'../ev','views','order-modx.html'));
 })
 app.get('/order-mody', (req, res)=>{ 
     // const params = { }
     // res.status(200).end(aboutus);
-    res.sendFile(path.join(__dirname ,'../ev','/views','order-mody.html'));
+    res.sendFile(path.join(__dirname ,'../ev','views','order-mody.html'));
 })
 
 
